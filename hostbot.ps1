718a3c5b96397a485bf249ebf012c316239a1a98
Enes Yalı <accounts@enesyali.com>
2026-05-14 20:09:52 +0300

    Create hostbot.ps1

---
+++ hostbot.ps1
@@ -0,0 +1,9 @@
+echo ""
+--------------------------------------------
+  Welcome to the FoodBot setup!
+This setup will guide you to installing the software.
+
+""
+git clone https://github.com/enesyalii/FoodBotClone.git
+
+echo "Cloned!"
